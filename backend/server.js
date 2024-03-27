4ba8dc41467232168e3d4b6d612ce2d45d44f80c
mrzkhrmn <mrzkhrmn@hotmail..com>
2024-03-27 23:51:53 +0300

    initialize and connecting to database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import authRouter from "./routes/authRoutes.js";
+import connectDatabase from "./db/connectDatabase.js";
+
+dotenv.config();
 
 const app = express();
+
+app.use(cors());
 
 app.use("/api/auth", authRouter);
 
 app.listen("9000", () => {
   console.log("Server is running on port 9000");
+  connectDatabase();
 });
