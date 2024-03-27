041e77eed90eadd262a765f33c38170f9251130f
mrzkhrmn <mrzkhrmn@hotmail..com>
2024-03-27 23:23:39 +0300

    setting up the auth api route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
+
+app.use("/api/auth", authRouter);
 
 app.listen("9000", () => {
   console.log("Server is running on port 9000");
